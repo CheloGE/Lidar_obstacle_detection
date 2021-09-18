5abe63376d23b67ee349866033f5baf2b437c938
MRGE <chelo_ge@hotmail.com>
2021-09-18 18:52:06 -0500

    feat: custom KD-tree insert algorithm implemented

--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -36,10 +36,31 @@ struct KdTree
 		delete root;
 	}
 
+	/* This is a helper function that implements a recursive approach to insert a node */
+	void insertHelper(Node** node, uint depth, std::vector<float> point, int id){
+
+		// Check if the tree is empty
+		if(*node == NULL){
+			*node = new Node(point, id);
+		}else{
+			// Calculate current dimension
+			uint cd = depth%2; // it is 2 because is a 2D example
+
+			if (point[cd]<((*node)->point[cd]))
+			{
+				insertHelper(&((*node)->left), depth+1, point, id);
+			}else{
+				insertHelper(&((*node)->right), depth+1, point, id);
+			}		
+		}
+
+	}
+
 	void insert(std::vector<float> point, int id)
 	{
-		// TODO: Fill in this function to insert a new point into the tree
+		// this function insert a new point into the tree
 		// the function should create a new node and place correctly with in the root 
+		insertHelper(&root, 0, point, id);
 
 	}
 
