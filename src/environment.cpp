14bbdb66151b5d637f3c657283668c3b15f80221
MRGE <chelo_ge@hotmail.com>
2021-10-10 13:49:51 -0500

    feat: adding a more complex LiDAR scenario

--- src/environment.cpp
+++ src/environment.cpp
@@ -79,9 +79,20 @@ void simpleHighway(pcl::visualization::PCLVisualizer::Ptr &viewer)
             Box box = PPC->BoundingBox(cluster);
             renderBox(viewer, box, clusterId);
         }
-         
+
         ++clusterId;
     }
+}
+
+void cityBlock(pcl::visualization::PCLVisualizer::Ptr &viewer)
+{
+    // ----------------------------------------------------
+    // -----Open 3D viewer and display City Block     -----
+    // ----------------------------------------------------
+
+    ProcessPointClouds<pcl::PointXYZI> *pointProcessorI = new ProcessPointClouds<pcl::PointXYZI>();
+    pcl::PointCloud<pcl::PointXYZI>::Ptr inputCloud = pointProcessorI->loadPcd("../src/sensors/data/pcd/data_1/0000000000.pcd");
+    renderPointCloud(viewer, inputCloud, "inputCloud");
 }
 
 //setAngle: SWITCH CAMERA ANGLE {XY, TopDown, Side, FPS}
@@ -121,7 +132,8 @@ int main(int argc, char **argv)
     pcl::visualization::PCLVisualizer::Ptr viewer(new pcl::visualization::PCLVisualizer("3D Viewer"));
     CameraAngle setAngle = XY;
     initCamera(setAngle, viewer);
-    simpleHighway(viewer);
+    // simpleHighway(viewer);
+    cityBlock(viewer);
 
     while (!viewer->wasStopped())
     {
